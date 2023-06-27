d2a0681fea34d9f3d8495808724ce842708940a7
pedro <pcgbernardes@gmail.com>
2023-06-27 13:55:26 -0400

    create variable port

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ mod server;
 mod website_handler;
 
 fn main() {
+    let port = 3000;
+    
     let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
     let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
-    let server = Server::new("127.0.0.1:3000".to_string());
+    let server = Server::new(format!("127.0.0.1:{}", port));
     server.run(WebsiteHandler::new(public_path));
 }
